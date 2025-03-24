e375f918ac63b811add1edbef51d3d61b2debb4f
krane <christian.kranabetter@protonmail.com>
2025-03-24 13:13:10 +0100

    beautify projects

--- app/components/form/projects.vue
+++ app/components/form/projects.vue
@@ -9,74 +9,83 @@ const state = reactive({
 		:state="state"
 		class="flex flex-col gap-8 m-4">
 		<UFormField>
-			<template
-				v-for="(_, index) in state.projects"
-				:key="index">
-
-				<FormCollapsible :label="state.projects[index]!.name || 'New Project'" class="my-2">
-					<div class="flex flex-col gap-1 not-first:mt-4 mb-4">
-						<div class="flex items-center gap-1">
-							<FormTooltip text="Name">
-								<UInput
-									v-model="state.projects[index]!.name"
-									class="grow"
-									variant="soft"
-									placeholder="Project Name" />
-							</FormTooltip>
-							<UFormField>
-								<UCheckbox v-model="state.projects[index]!.openSource" label="Open Source" />
-							</UFormField>
-						</div>
-						<FormTooltip text="URL">
-							<UInput
-								v-model="state.projects[index]!.url!"
-								class="grow"
-								variant="soft"
-								placeholder="https://example.com/" />
-						</FormTooltip>
-						<FormTooltip text="Repository">
-							<div class="flex gap-1">
-								<FormTechSelectMenu git-platforms-only placeholder="Repository Platform" single />
-								<UInput
-									v-model="state.projects[index]!.repoLink!.url!"
-									class="grow"
-									variant="soft"
-									placeholder="https://github.com/example/repo.git" />
+			<div class="flex flex-col gap-8">
+				<div
+					v-for="(_, index) in state.projects"
+					:key="index"
+					class="flex items-center grow gap-1">
+					<FormCollapsible
+						v-model:open="state.projects[index]!.collapsibleOpen"
+						:label="state.projects[index]!.name || 'New Project'"
+						class="grow">
+						<div class="flex flex-col gap-1">
+							<div class="flex items-center gap-1">
+								<FormTooltip text="Name">
+									<UInput
+										v-model="state.projects[index]!.name"
+										class="grow"
+										variant="soft"
+										placeholder="Project Name" />
+								</FormTooltip>
+								<UFormField>
+									<UCheckbox v-model="state.projects[index]!.openSource" label="Open Source" />
+								</UFormField>
 							</div>
-						</FormTooltip>
-						<FormTooltip text="Description">
-							<UTextarea
-								v-model="state.projects[index]!.description"
-								class="w-full"
-								variant="soft"
-								placeholder="Project Description"
-								autoresize />
-						</FormTooltip>
-						<div class="flex items-center gap-1">
-							<FormTechSelectMenu v-model="state.projects[index]!.technologies" class="grow" />
-							<UFormField>
-								<FormClearInputButton :fn="() => { state.projects[index]!.technologies = [] }" trailing label="Clear Technologies" />
-							</UFormField>
+							<FormTooltip text="URL">
+								<UInput
+									v-model="state.projects[index]!.url!"
+									class="grow"
+									variant="soft"
+									placeholder="https://example.com/" />
+							</FormTooltip>
+							<FormTooltip text="Repository">
+								<div class="flex gap-1">
+									<FormTechSelectMenu git-platforms-only placeholder="Repository Platform" single />
+									<UInput
+										v-model="state.projects[index]!.repoLink!.url!"
+										class="grow"
+										variant="soft"
+										placeholder="https://github.com/example/repo.git" />
+								</div>
+							</FormTooltip>
+							<FormTooltip text="Description">
+								<UTextarea
+									v-model="state.projects[index]!.description"
+									class="w-full"
+									variant="soft"
+									placeholder="Project Description"
+									autoresize />
+							</FormTooltip>
+							<div class="flex items-center gap-1">
+								<FormTechSelectMenu v-model="state.projects[index]!.technologies" class="grow" />
+								<UFormField>
+									<FormClearInputButton :fn="() => { state.projects[index]!.technologies = [] }" trailing label="Clear Technologies" />
+								</UFormField>
+							</div>
+							<div
+								v-if="state.projects[index]!.technologies.length > 0"
+								class="flex flex-wrap gap-1">
+								<UIcon
+									v-for="(t, tIndex) in state.projects[index]!.technologies"
+									:key="tIndex"
+									:name="t.icon" />
+							</div>
 						</div>
-						<div
-							v-if="state.projects[index]!.technologies.length > 0"
-							class="flex flex-wrap gap-1">
-							<UIcon
-								v-for="(t, tIndex) in state.projects[index]!.technologies"
-								:key="tIndex"
-								:name="t.icon" />
-						</div>
-						<div class="flex">
-							<div class="grow" />
-							<FormClearInputButton :fn="() => { state.projects.splice(index, 1) }" trailing label="Remove Project" />
-						</div>
+					</FormCollapsible>
+
+					<div class="flex h-full items-center">
+						<FormModifyButtons
+							v-model="state.projects"
+							:index
+							:vertical="state.projects[index]!.collapsibleOpen || true" />
 					</div>
-				</FormCollapsible>
-			</template>
+				</div>
+			</div>
 
 			<div class="flex">
 				<UButton
 					label="Add Project"
+					variant="soft"
 					class="mt-2 mx-auto"
 					@click="() => { state.projects.push({ name: '', description: '', url: '', repoLink: { name: '', url: '' }, technologies: [] }) }" />
 			</div>
