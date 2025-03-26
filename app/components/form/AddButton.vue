8b770cd93ffa7d95ef5bbb85abe4869a889c4811
krane <christian.kranabetter@protonmail.com>
2025-03-26 09:35:59 +0100

    add raw option for add button

--- app/components/form/AddButton.vue
+++ app/components/form/AddButton.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 const model = defineModel<object[] | string[]>()
-const {label, defaultValueGetter} = defineProps<{
+const {label, defaultValueGetter, raw = false} = defineProps<{
 	label: string
 	defaultValueGetter: () => object | string
+	raw?: boolean
 }>()
 </script>
 
 <template>
-	<div class="flex">
+	<UButton
+		v-if="raw"
+		:label
+		variant="soft"
+		class="mx-auto cursor-pointer"
+		@click="() => { model!.push(defaultValueGetter()) }"/>
+	<div v-else class="flex">
 		<UButton
 			:label
 			variant="soft"
