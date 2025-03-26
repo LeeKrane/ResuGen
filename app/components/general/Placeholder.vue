b8b553f5ae992b1622185f541f0ebed5611790ff
krane <christian.kranabetter@protonmail.com>
2025-03-26 23:09:55 +0100

    modify placeholder for lower min-w

--- app/components/general/Placeholder.vue
+++ app/components/general/Placeholder.vue
@@ -1,5 +1,5 @@
 <template>
-	<UContainer
+	<div
 		class="rounded-lg border border-dashed border-(--ui-border-accented) bg-[length:8px_8px] bg-[linear-gradient(-45deg,var(--ui-bg-elevated)_10%,var(--ui-bg)_10%,var(--ui-bg)_50%,var(--ui-bg-elevated)_50%,var(--ui-bg-elevated)_60%,var(--ui-bg)_60%)]"
 	/>
 </template>
