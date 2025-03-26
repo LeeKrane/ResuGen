9bafbc98c2202a334ea5636038d8a64aff6806a1
krane <christian.kranabetter@protonmail.com>
2025-03-26 09:35:44 +0100

    add particles

--- app/components/general/Particles.vue
+++ app/components/general/Particles.vue
@@ -1,57 +1,72 @@
 <script setup lang="ts">
 const options = {
-	"particles": {
-		"number": {
-			"value": 100,
-			"density": {
-				"enable": true,
-				"value_area": 800
-			}
-		},
-		"color": {
-			"value": "#b8b8b8"
-		},
-		"shape": {
-			"type": "circle",
-		},
-		"opacity": {
-			"value": 0.3,
-			"random": true,
-			"anim": {
-				"enable": true,
-				"speed": 1,
-				"opacity_min": 0,
-				"sync": false
-			}
-		},
-		"size": {
-			"value": 1.5,
-			"random": true,
-		},
-		"line_linked": {
-			"enable": true,
-			"distance": 1200,
-			"color": "#b8b8b8",
-			"opacity": 1,
-			"width": 1
-		},
-		"move": {
-			"enable": true,
-			"speed": 0.3,
-			"direction": "none",
-			"random": true,
-			"straight": false,
-			"out_mode": "out",
-			"bounce": false,
+	fpsLimit: 60,
+	fullScreen: {
+		enable: false,
+		zIndex: -50
+	},
+	background: {
+		color: {
+			value: "transparent"
 		}
 	},
+	particles: {
+		number: {
+			value: 200,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		},
+		links: {
+			enable: true,
+			distance: 100,
+			color: "#b8b8b8",
+			opacity: 0.2,
+			width: 0.5
+		},
+		color: {
+			value: "#b8b8b8"
+		},
+		shape: {
+			type: "circle",
+		},
+		opacity: {
+			value: 0.3,
+			random: true,
+			anim: {
+				enable: true,
+				speed: 1,
+				opacity_min: 0,
+				sync: false
+			}
+		},
+		size: {
+			value: 1.5,
+			random: true,
+		},
+		move: {
+			enable: true,
+			speed: 0.3,
+			direction: "none",
+			random: true,
+			straight: false,
+			out_mode: "out",
+			bounce: false,
+		}
+	},
+	interactivity: {
+		events: {
+			resize: true
+		}
+	}
 }
 </script>
 
 <template>
 	<NuxtParticles
-		class="-z-50"
+		class="fixed inset-0 -z-50"
 		id="tsparticles"
 		:options="options"
-	></NuxtParticles>
+	/>
 </template>
