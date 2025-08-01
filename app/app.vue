e250b81cb6462fc1d7b59bc2b01e8658f8328439
Uniixon <theprofigamerpdymailbox@gmail.com>
2025-08-01 16:26:09 +0200

    add global transition

--- app/app.vue
+++ app/app.vue
@@ -8,9 +8,9 @@
 </template>
 
 <style lang="css">
-* {
-  transition-property: filter, color, box-shadow, background-color, border-color, text-decoration-color, fill, stroke, --tw-gradient-from, --tw-gradient-via, --tw-gradient-to, text-color;
-  transition-timing-function: var(--default-transition-timing-function); /* z. B. cubic-bezier(0.4, 0, 0.2, 1) */
-  transition-duration: 10s; /* z. B. 150ms */
+*, ::before, ::after {
+transition-property: filter, color, box-shadow, background-color, border-color, text-decoration-color, fill, stroke, --tw-gradient-from, --tw-gradient-via, --tw-gradient-to;
+transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+transition-duration: 250ms;
 }
-</style>
+</style> 
