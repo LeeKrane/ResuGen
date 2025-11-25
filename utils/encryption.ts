12df65b9995035e8b3ecc163b4f82be335713e03
krane <chris@krane.dev>
2025-11-25 17:54:13 +0100

    add encryption service file header

---
+++ utils/encryption.ts
@@ -0,0 +1,6 @@
+/**
+ * Client-side encryption service for ResuGen
+ * Provides AES-GCM encryption with PBKDF2 key derivation
+ * All sensitive data is encrypted before transmission to the server
+ */
+
